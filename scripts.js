5359f54876b3ff9c6eb714fa7c4d74ca8c2fde73
Manav Misra <manavm@visionwebsoft.com>
2022-10-05 17:19:08 -0500

    Delete scripts.js

--- scripts.js
+++
@@ -1,6 +0,0 @@
-// Enter a description of this program here.
-
-/**
- * Write some pseudo code steps here.
- * Use those steps as your `commit` messages (and delete them from here as you complete a step.)
- */
